5e58a3c9cf2f039ce90f86d09276fca22cabf6b9
Wagner Ramos <155022577+Ramoswagner@users.noreply.github.com>
2026-03-12 23:33:03 -0300

    Update ModeloBase.js

--- js/models/ModeloBase.js
+++ js/models/ModeloBase.js
@@ -1,72 +1,14 @@
 // js/models/ModeloBase.js
-export class ModeloBase {
-    constructor(nome, descricao) {
+class ModeloBase {
+    constructor(nome, corFundo, corTexto, classeCSS) {
         this.nome = nome;
-        this.descricao = descricao;
+        this.corFundo = corFundo;
+        this.corTexto = corTexto;
+        this.classeCSS = classeCSS;
     }
 
-    // Métodos obrigatórios - TODOS os modelos devem implementar
-    gerarSlideCapa(pres, G, T) {
-        throw new Error('Implemente gerarSlideCapa');
-    }
-
-    gerarSlideSumario(pres, G, T) {
-        throw new Error('Implemente gerarSlideSumario');
-    }
-
-    gerarSlidePanorama(pres, G, T) {
-        throw new Error('Implemente gerarSlidePanorama');
-    }
-
-    gerarSlideDivisor(pres, projeto, G, T, indice) {
-        throw new Error('Implemente gerarSlideDivisor');
-    }
-
-    gerarSlideObjetivo(pres, projeto, C) {
-        throw new Error('Implemente gerarSlideObjetivo');
-    }
-
-    gerarSlideEquipe(pres, projeto, C) {
-        throw new Error('Implemente gerarSlideEquipe');
-    }
-
-    gerarSlideEtapas(pres, projeto, C) {
-        throw new Error('Implemente gerarSlideEtapas');
-    }
-
-    gerarSlideMarcos(pres, projeto, C) {
-        throw new Error('Implemente gerarSlideMarcos');
-    }
-
-    gerarSlideIndicadores(pres, projeto, C) {
-        throw new Error('Implemente gerarSlideIndicadores');
-    }
-
-    gerarSlideResultados(pres, projeto, C) {
-        throw new Error('Implemente gerarSlideResultados');
-    }
-
-    gerarSlideAntesDepois(pres, projeto, C) {
-        throw new Error('Implemente gerarSlideAntesDepois');
-    }
-
-    gerarSlideEvidencias(pres, projeto, C) {
-        throw new Error('Implemente gerarSlideEvidencias');
-    }
-
-    gerarSlideRiscos(pres, projeto, C) {
-        throw new Error('Implemente gerarSlideRiscos');
-    }
-
-    gerarSlideLicoes(pres, projeto, C) {
-        throw new Error('Implemente gerarSlideLicoes');
-    }
-
-    gerarSlideDesafios(pres, projeto, C) {
-        throw new Error('Implemente gerarSlideDesafios');
-    }
-
-    gerarSlideEncerramento(pres, G, T) {
-        throw new Error('Implemente gerarSlideEncerramento');
+    aplicar() {
+        // Método base que pode ser sobrescrito
+        console.log(`Aplicando modelo: ${this.nome}`);
     }
 }
