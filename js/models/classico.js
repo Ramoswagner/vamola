d067f948ff3c9700fd1f61ff0c3e26e108a26983
Wagner Ramos <155022577+Ramoswagner@users.noreply.github.com>
2026-03-13 00:47:39 -0300

    Update classico.js

--- js/models/classico.js
+++ js/models/classico.js
@@ -1,6 +1,6 @@
 // js/models/classico.js
 class ModeloClassico extends ModeloBase {
     constructor() {
-        super('Clássico', '#f4e9d8', '#5a3e2b', 'classico');
+        super('Clássico');
     }
 }
