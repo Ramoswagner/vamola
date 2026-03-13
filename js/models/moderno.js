0e4cb7e774df56d944df03c987a79d0ddfed9bf0
Wagner Ramos <155022577+Ramoswagner@users.noreply.github.com>
2026-03-13 00:48:00 -0300

    Update moderno.js

--- js/models/moderno.js
+++ js/models/moderno.js
@@ -1,6 +1,6 @@
 // js/models/moderno.js
 class ModeloModerno extends ModeloBase {
     constructor() {
-        super('Moderno', '#1e2b3a', '#e0e0e0', 'moderno');
+        super('Moderno');
     }
 }
