8f975ef1ec81db14adad1ad3f3e9bc9bbd8cc5c4
Wagner Ramos <155022577+Ramoswagner@users.noreply.github.com>
2026-03-13 00:48:19 -0300

    Update minimalista.js

--- js/models/minimalista.js
+++ js/models/minimalista.js
@@ -1,6 +1,6 @@
 // js/models/minimalista.js
 class ModeloMinimalista extends ModeloBase {
     constructor() {
-        super('Minimalista', '#ffffff', '#333333', 'minimalista');
+        super('Minimalista');
     }
 }
