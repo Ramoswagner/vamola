207ed6b3f0619d1dc5636e525cb3e9202dd4b3cb
Wagner Ramos <155022577+Ramoswagner@users.noreply.github.com>
2026-03-13 00:49:07 -0300

    Update index.js

--- js/models/index.js
+++ js/models/index.js
@@ -1,10 +1,7 @@
 // js/models/index.js
-// Cria instâncias dos modelos para uso global
+// Cria instâncias dos modelos para uso no main.js
 const modelos = {
     classico: new ModeloClassico(),
     moderno: new ModeloModerno(),
     minimalista: new ModeloMinimalista()
 };
-
-// Se precisar que o objeto seja acessível em outros arquivos, ele está no escopo global (window)
-// O main.js pode acessar diretamente a variável 'modelos'
